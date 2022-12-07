9c89d9cf5338a7b251f819a4cc6b68aba549e686
evanrk <evanrklein@gmail.com>
2022-12-07 15:22:56 -0500

    Update

--- LinearDiophantineEquations.py
+++
@@ -1,9 +0,0 @@
-# equation = input("Enter your linear diophantine equation in the input ax + by = c  ")
-
-def gcd(a, b, iter=0): return ((a+b) if a+b != 1 else 1) if not a or not b else gcd(b, a%b, iter+1) # function from before that finds the gcd of two numbers
-# def reverseEuclidAlg(a, b, iter=0):
-
-print(gcd(252, 198))
-
-# def diophantineSolver(a, b, c):
-#     gcd = gcd(a, b)
