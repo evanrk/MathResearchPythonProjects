9647532cc5012ba14fec2b076d558ae3bd2f22a1
evanrk <evanrklein@gmail.com>
2022-12-07 18:47:53 -0500

    :D

--- LinearDiophantineEquations.py
+++ LinearDiophantineEquations.py
@@ -3,43 +3,34 @@ class Coef():
         self.coef = coef
         self.expression = expression
 
-def simplifyMultiplication(expression):  # helper function
+
+# def simplify(expression):
+#     print(toList(expression))
+#     if type(expression) == list:
+#         simplifiedList = []
+#         for element in expression:
+
+#         # return 
+#         # return [simplify(element) for element in expression]
+#     if type(expression.expression) == Coef:
+#         expression.expression.coef *= expression.coef
+#         return simplify(expression.expression)
+#     else:
+#         return expression
+
+def simplify(expression):
     if type(expression.expression) == list:
         scaledList = []
         for element in expression.expression:
-            simplifiedElement = simplifyMultiplication(element)
-            scaledElement = Coef(simplifiedElement.coef *
-                                 expression.coef, simplifiedElement.expression)
+            simplifiedElement = simplify(element)
+            scaledElement = Coef(simplifiedElement.coef * expression.coef, simplifiedElement.expression)
             scaledList.append(scaledElement)
         return scaledList
     elif type(expression.expression) == Coef:
         expression.expression.coef *= expression.coef
-        return simplifyMultiplication(expression.expression)
+        return simplify(expression.expression)
     else:
         return expression
-
-
-# def simplifyMultiplication(expression):  # helper function
-#     if type(expression) == list:
-#         scaledList = []
-#         for element in expression.expression:
-#             scaledList.append(simplifyMultiplication(element))
-#         return scaledList
-#     elif type(expression.expression) == Coef:
-#         expression.expression.coef *= expression.coef
-#         return simplifyMultiplication(expression.expression)
-#     else:
-#         return expression
-
-
-# def simplifyMultiplication(expression):
-#     if type(expression.expression) == list:
-
-
-
-def simplify(expression):
-    multiplicationSimplified = simplifyMultiplication(expression)
-    return multiplicationSimplified
 
 
 def toList(expression):
@@ -52,12 +43,9 @@ def toList(expression):
 
 
 # testExpression1 = Coef(5, Coef(2, Coef(1, Coef(3, "x"))))
-testExpression1 = Coef(5, [Coef(1, [
-    Coef(2, Coef(7, "x")),
-    Coef(4, [
-        Coef(7, "x"),
-        Coef(5, "x")
-    ])
-])])
+testExpression1 = Coef(5, Coef(1, [
+    Coef(2, Coef(7, "y")),
+    Coef(4, "x")
+]))
 # 5 * ( 1 * ( 2 * (7y + 4x)))
 print(toList(simplify(testExpression1)))
