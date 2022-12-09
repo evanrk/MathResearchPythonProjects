713bfc44837b7e52db60245d16c287b84d160294
Evan Klein <evanrklein@gmail.com>
2022-12-08 19:25:23 -0500

    updated functions plus change in focus of program

--- LinearDiophantineEquations.py
+++ LinearDiophantineEquations.py
@@ -2,7 +2,23 @@ class Coef():
     def __init__(self, coef, expression):
         self.coef = coef
         self.expression = expression
-    
+
+def replaceExpression(expression, searchTerm, replaceTerm):
+    print(replaceTerm)
+    if type(expression) == list:
+        return [(replaceExpression(element, searchTerm, replaceTerm)) for element in expression]
+        # for element in expression:
+        #     return replaceExpression(element, searchTerm, replaceTerm)
+    elif type(expression) == Coef:
+        return Coef(expression.coef, replaceExpression(expression.expression, searchTerm, replaceTerm))
+    else:
+        if expression == searchTerm:
+            return replaceTerm
+        else:
+            return expression
+
+ 
+testExpression3 = Coef(2, [Coef(3, 1), Coef(3, 1)])
 
 def simplifyMultiplied(expression):
     if type(expression.expression) == list:
@@ -29,8 +45,6 @@ def simplify(expression):
     multiplySimplified = simplifyMultiplied(expression)
     variables = []
 
-    print(toList(multiplySimplified))
-
     for i in multiplySimplified:
         variables.append(i.expression)
 
@@ -45,9 +59,19 @@ def simplify(expression):
                 
 
 
+# def toList(expression):
+#     if type(expression) == list:
+#         return [(toList(element) if type(element) != int else element) for element in expression]
+#     elif type(expression.expression) == Coef:
+#         return [expression.coef, toList(expression.expression)]
+#     else:
+#         return [expression.coef, expression.expression]
+
+
 def toList(expression):
+    print(expression)
     if type(expression) == list:
-        return [(toList(element) if type(element) != int else [element]) for element in expression]
+        return [(toList(element) if type(element) in (list, Coef) else element) for element in expression]
     elif type(expression.expression) == Coef:
         return [expression.coef, toList(expression.expression)]
     else:
@@ -59,5 +83,6 @@ testExpression1 = Coef(5, Coef(1, [
     Coef(2, [Coef(7, [Coef(3, "y"), Coef(1, "y")]), Coef(9, "y")]),
     Coef(4, "x")
 ]))
-# 5 * ( 1 * ( 2 * (7y + 4x)))
-print(toList(simplify(testExpression1)))
+
+# print(toList(replaceExpression(testExpression3, 1, "x")))
+print(toList(testExpression1))
